679ba8f8d13d4b85556fcd84c5ef1010c48d88e2
F.I.D.O. <david.fido.fiedler@gmail.com>
2015-09-08 20:39:05 +0200

    TB malý

---
+++ TB_maly.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="Content-Style-Type" content="text/css">
+	<meta http-equiv="Content-Language" content="cs">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta name="geo.country" content="CZ">
+	<meta name="keywords" content="skauti Plzeň,skauting Plzen,Vločka Plzeň,skauti předškoláci,skautský oddíl Plzeň">
+	<meta name="Rating" content="General">
+	<meta name="googlebot" content="snippet,archive">
+	<meta name="description" content="28. oddíl Vločka - Kontakty">
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<link rel="image_src" href="http://vlocka.skauting.cz/images/ikonyweb/logo3.png">
+	<link href="http://vlocka.skauting.cz/images/ikonyweb/logo-mini3.ico" rel="Shortcut Icon">
+	<title>28. oddíl Vločka Plzeň</title>
+</head>
+
+<body>
+	<div id="hlavni">
+		<?php
+		require 'php/databaze.php';
+		require 'php-vzhled/zahlavi-default.php';
+		require 'php-vzhled/horejsek.php';
+		require 'php-vzhled/vlevo_zacatek.php';?>
+		<?php 
+		require 'php-vzhled/vlevo_konec.php';
+		?>
+		
+		<div class="obsah">
+			<h1>Táborové body vlčat a světlušek</h1>
+				<p style="position: relative; top: -45px; left: 480px;">Aktualizováno 8. září 2015</p>
+				<p>I pro letošní rok jsme se rozhodli, jako podmínku účasti na letním táboře využít táborové body. 
+                    Táborové body děti získávají za účast na oddílovkách a výpravách. Dohromady jich za celý školní rok
+                    mohou získat více než 30. Pro účast na táboře je třeba získat min. 20 bodů, což by nemělo být 
+                    problémem pro nikoho, kdo má o členství v oddíle opravdový zájem. Letos již nebudeme dělat žádné 
+                    vyjímky, na tábor pojedou opravdu jen ti, kteří táborové body splnili.</p>
+				<br />
+				
+<!--				<p style="margin-bottom: 0px">
+				 	<a style="font-size: 25px" target="_blank" href="https://docs.google.com/spreadsheet/ccc?key=0ArXSkomV3fw_dFY3b0pnRFNNUGx0OE5SeWZ4REZPV0E&usp=sharing">
+				 	Podrobný rozpis táborových bodů</a>
+				</p>-->
+				<p>Bodové ohodnocení jednotlivých akcí: </p>
+                <ul>
+                    <li>Oddílovka - 1 TB</li> 
+                    <li>Jednodenní výprava - 1 TB</li> 
+                    <li>Šestková výprava, Oddílové Vánoce, Závod Stopou bratra Šiwy - 3 TB</li> 
+                    <li>Velikonočky, Podzimky - 5 TB</li> 
+                </ul>
+                <p>Počty táborových bodů budeme pravidelně aktualizovat.</p>
+		</div>
+		<?php require 'php-vzhled/spodek.php'?>			
+	</div>
+</body>
+</html>
