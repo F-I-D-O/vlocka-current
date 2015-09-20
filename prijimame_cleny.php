bfbe8d49ee6ba1ece803b22773d6ee57b2cc30fc
Jakub <Jakub@Jakub>
2015-09-20 12:49:37 +0200

    O nas, prijimame cleny, kontakt

--- prijimame_cleny.php
+++ prijimame_cleny.php
@@ -31,76 +31,62 @@
 		
 		<div class="obsah">				
 			<h1>Přijímáme nové členy!</h1> 
-				<p style="position: relative; top: -45px; left: 500px;">Aktualizováno 6. srpna 2013</p>
+				<p style="position: relative; top: -45px; left: 500px;">Aktualizováno 20. září 2015</p>
 				<h2>Chceš:</h2>
 				 	<ul style="font-weight: bold">
-						<li>najít opravdové kamarády a patřit do té správné party?
-							<span style="font-weight: normal">...a ne jen o ní číst v knížkách!?</span></li>
-						<li>vylézt na horolezeckou stěnu, sjet po lanovce, projet se na lodičce, zažít dobrodružství?
-							<span style="font-weight: normal">...a poznat sám sebe?</span></li>
-						<li>vyzkoušet si různé rukodělky, zahrát si divadlo, trochu sportovat, připravit něco pro ostatní?
-							<span style="font-weight: normal">...a najít si, co tě baví a v čem jsi dobrý?</span></li>
-						<li>jezdit na výlety, tábory, závody?
-							<span style="font-weight: normal">...prostě mít o čem vyprávět a na co vzpomínat?</span></li>
-						<li>zkusit si, jaké je to být kouzelnicí, rytířem, skřítkem, elfem?
-							<span style="font-weight: normal">...a nejen v počítačové hře?</span></li>
+						<li>najít opravdové kamarády a patřit do té správné party 
+							<span style="font-weight: normal">a ne jen o ní číst v knížkách!?</span></li>
+						<li>vylézt na horolezeckou stěnu, sjet po lanovce, projet se na lodičce, zažít dobrodružství 
+							<span style="font-weight: normal"></span></li>
+						<li>vyzkoušet si různé rukodělky, zahrát si divadlo, trochu sportovat, připravit něco pro ostatní
+							<span style="font-weight: normal">a najít si, co tě baví a v čem jsi dobrý?</span></li>
+						<li>jezdit na výlety, tábory, závody,  
+							<span style="font-weight: normal">prostě mít o čem vyprávět a na co vzpomínat?</span></li>
+						<li>zkusit si, jaké je to být kouzelnicí, rytířem, skřítkem, elfem  
+							<span style="font-weight: normal">a to nejen v počítačové hře?</span></li>
 						<li>bavit se, smát se, povídat si?</li>
 						<li>rozvíjet své schopnosti a dovednosti, vyzkoušet si co dokážeš?</li>
 						<li>mít hodné vedoucí…</li>
 					</ul>
-				<h2 id="prijem"> Přijímáme nové děti – holky a kluky od 5 do 12 let od září 2013!</h2>
-					<h3>Přijď se podívat na některou schůzku! (začínají od pondělí 9. 9.)</h3>
-						<h4>pondělí – 16 - 18:30h</h4>
+				<h2 id="prijem"> Přijímáme nové skauty – holky a kluky od 10 do 13 let!</h2>
+					<h3>Přijď se podívat na některou schůzku!</h3>
+						<h4>středa – 16:00 – 19:00</h4>
 							<ul>
-								<li>předškoláci (kluci i holky 5 - 7 let, MŠ a 1. třída) - <u></u></li>
-								<li>fialová šestka světlušek (holky 6 - 9 let, 1. - 3. třída) - <u></u></li>
-							 	<li>hnědá šestka vlčat (kluci 7 – 10 let,  2. - 4. třída) - <u></u></li>						 	
+                                                            <li>Lasičky (holky 10 – 12 let)</li>
+                                                            <li>Lišáci (kluci 10 - 12 let)</li>					 	
 							</ul> 	
-				 		<h4>středa –  16 - 18:30h</h4>
+				 		<h4>čtvrtek – 16:00 - 19:00h</h4>
 					 		<ul>
-					 			<li>předškoláci (kluci i holky 5 - 7 let, MŠ a 1. třída) - <u></u></li>
-					 			<li>červená šestka světlušek (holky 6 – 8 let, 1.- 2. třída) - <u></u></li>
-							 	<li>modrá šestka světlušek (holky 8 – 10 let, 3.- 4. třída) - <u></u></li>
-							 	<li>černá šestka vlčat (kluci 6 – 9 let, 1. - 3. třída) - <u></u></li>
-							 	<li>Lišáci (kluci 10 - 12 let,  5. - 6. třída) <u>končí až v 19:00!!</u> - 
-							 		<u></u>
-							 	</li>
+                                                            <li>Káňata (kluci 11 – 13 let) </li>
+							 	
 							</ul>
-	 				 	<h4>čtvrtek –  16 - 19:00h</h4>
-	 				 		<ul>
-	 				 			<li>oranžová šestka vlčat (kluci 7 – 9 let, 2. - 3. třída) - <u></u></li>
-							 	<li>Káňata (kluci 11 - 14 let, 6. - 8. třída) - <u></u></li>
-					 			<li>Rybičky (holky 10 - 14 let, 5. - 8. třída) - <u></u></li>
-							</ul>
+                                                
 					 		<br>
-					 		<p>V případě že je šestka/družina plně obsazena, přijímáme náhradníky - v případě, že některé dítě 
-					 			ukončí činnost, budeme Vás kontaktovat.</p>
-					 		<p>Sraz je vždy v 16h  na konečné tramvaje č. 1 v Bolevci,  návrat   vlčat, světlušek a předškoláků  
-					 			ze schůzky je  v 18,30h tamtéž, skauti v 19h.</p>					 
+					 		<p>V případě že je družina plně obsazena, přijímáme náhradníky - v případě, že
+                                                            některé dítě ukončí činnost, budeme Vás kontaktovat.</p>
+                                                        
+					 		<p>Sraz je vždy v 16h na konečné tramvaje č. 1 v Bolevci, návrat  v 19h tamtéž.</p>	
+                                                        
 					 	<h4>Kontakt pro nováčky</h4>	 
-						 	<p><b>Pro děti do 7 let (půjdou do předškoláků):</b> mobil: 720 224 159 
-						 			(Lenka Chvalová - Sojka) 
-						 			<span style="position: relative; Left: 370px;"> mail: lenkasojka@gmail.com</span>
-						 			</p>
-					 		<p><b>Pro děti od 7 do 11 let (půjdou do Vlčat/světlušek):</b> mobil.: 605 472 772 
-					 			(Šárka Sedláčková - Syky) 
-					 			<span style="position: relative; Left: 370px;">mail: vlocka.info@gmail.com</span>
-					 			</p>
-					 		<p><b>Pro děti nad 11 let (půjdou do skautů/skautek):</b> mobil.: 737 472 531 
-					 			(David Fiedler - Fido), 
-					 			<span style="position: relative; Left: 370px;">mail: david.fido.fiedler@gmail.com</span>
-					 			</p>
+                                                        <ul>
+                                                            <li>Michal Straka – Drak, mobil: 773 608 499, mail: michalstraka1236@gmail.com (vůdce skautů)</li>
+                                                            <li>Šárka Sedláčková - Syky, mobil: 605 472 772, mail: vlocka.info@gmail.com (vůdkyně oddílu)</li>
+                                                        </ul>
+
 						<h4>Jak se přihlásit?</h4>
 							<ul>
-	  					 		<li>přijďte se podívat na některou ze zářijových schůzek! (začínají v týdnu od 10. září)</li>
-	    				 		<li>ozvěte se nám mailem, telefonicky apod.!</li>
-	     				 		<li>přijďte na Odpoledne jako hroch a Strašidelný les!</li>
-	        			 		<li>...vypadá to složitě, ale je to tak jednoduché - stačí přijít!!</li>
-        			 		</ul>
+                                                            <li>přijďte se podívat na některou ze zářijových schůzek!</li>
+                                                            <li>ozvěte se nám mailem, telefonicky apod.!</li>
+                                                            <li>přijďte na Odpoledne jako hroch a Strašidelný les!</li>
+                                                            <li>...vypadá to složitě, ale je to tak jednoduché - stačí přijít!!</li>
+
+                                                        </ul>
+                                                        
    					 	<h4>Kdo k nám chodí?</h4>
-  					 		<p>Kluci a holky od pěti let nejen z Košutky, Bolevce, Vinic, ale i z Bor, Slovan, středu města, 
-  					 			Roudné, Doubravky a dalších částí Plzně. Máme i několik mimoplzeňských členů - záleží jen na 
-  					 			tom odkud jsou ochotni dojíždět (nebo spíš na ochotě rodičů je k nám vozit...)</p>
+                                                        <br>
+  					 		<p>Kluci a holky od pěti let nejen z Košutky, Bolevce, Vinic, ale i z Bor, Slovan, středu města,
+                                                            Roudné, Doubravky a dalších částí Plzně. Máme i několik mimoplzeňských členů - záleží
+                                                            jen na tom odkud jsou ochotni dojíždět (nebo spíš na ochotě rodičů je k nám vozit...)</p>
 							<p>Těšíme se na tebe!<p>
 		</div>
 		<?php require 'php-vzhled/spodek.php'?>			
