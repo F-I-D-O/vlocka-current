1239850eda773e75c6219df2acc0395b89e03c04
Ing. David Fiedler <fiedlda1@fel.cvut.cz>
2017-04-12 21:24:58 +0200

    fotokronika 2017

--- fotokronika.php
+++ fotokronika.php
@@ -165,6 +165,8 @@ require 'php/funkce.php';
 			/* Výpis menu */
 			else{ ?>
 				<h1>Fotokronika</h1>
+                    <h2>2017</h2>
+	    				<a href="fotokronika.php?rok=2017"><img src="fotky/fotokronika/2017.JPG" alt="rok 2017" width="650px"/></a>
 					<h2>2016</h2>
 	    				<a href="fotokronika.php?rok=2016"><img src="fotky/fotokronika/2016.JPG" alt="rok 2016" width="650px"/></a>
 					<h2>2015</h2>
