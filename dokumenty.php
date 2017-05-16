03638bdcce953aa8d456966db3a570bbbbd09fa6
Ing. David Fiedler <fiedlda1@fel.cvut.cz>
2017-05-16 09:51:09 +0200

    info tábor 2017

--- dokumenty.php
+++ dokumenty.php
@@ -49,15 +49,15 @@
 						Informace pro nováčky 2016
 					</a>
 				</p>
-				<!--<p>
+<!--				<p>
 					<a target="_blank" href="dokumenty/Informace-pro-rodiče-vlčat-a-světlušek-2015.pdf">
 						Informace pro rodiče včat a světlušek 2015
 					</a>
-				</p>
-<!--                <p><a  target="_blank" href="dokumenty/Seznam-věcí.pdf">Seznam věcí na tábor 2015</a></p>
-                <p><a  target="_blank" href="dokumenty/Druhé-info-tábor-2015.pdf">Další informace o táboře 2015</a></p>
-	 			<p><a  target="_blank" href="dokumenty/první-info.pdf">Informace o táboře 2015</a></p>
-	 			<p>
+				</p>-->
+                <!--<p><a  target="_blank" href="dokumenty/Seznam-věcí.pdf">Seznam věcí na tábor 2015</a></p>-->
+                <!--<p><a  target="_blank" href="dokumenty/Druhé-info-tábor-2015.pdf">Další informace o táboře 2015</a></p>-->
+	 			<p><a  target="_blank" href="dokumenty/První-informace-k-táboru.pdf">Informace o táboře 2017</a></p>
+<!--	 			<p>
 					Přihláška na tábor 2015 
 					<a target="_blank" href="dokumenty/prihlaska_tabor_2015.doc">DOC</a>
 					<a target="_blank" href="dokumenty/prihlaska_tabor_2015.pdf">PDF</a>
